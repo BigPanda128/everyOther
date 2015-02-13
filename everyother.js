0eb4b0afff69ff3461f371b23920be7a6927e21f
Ryan Postma <rpostma@sjccs142.saintjoe.edu>
2015-02-13 12:28:14 -0500

    extra code working

--- everyother.js
+++ everyother.js
@@ -1,9 +1,16 @@
-num = [1,2,3,4,5,6,7,8,9,10];
+var num = [1,2,3,4,5,6,7,8,9,10];
+var gap = 1;
 
 function everyOther(array) {
-	for(var i = 0; i < array.length; i += 2) {
+	for (var i = 0; i < array.length; i++) {
+		if (i % 2 == 0)
+			print(array[i]);
+	}
+}
+function gapIncrease(array) {
+	for(var i = 0; i < array.length; i += gap++) {
 		print(array[i]);
 	}
 }
-
 everyOther(num);
+gapIncrease(num);
